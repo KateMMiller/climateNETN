1d7a123d266f96865e684e3448809e724117c24d
KateMMiller <kathryn_miller@nps.gov>
2024-07-10 10:23:12 -0400

    added norm errors

--- scripts/compile_NOAA_normals_final.R
+++ scripts/compile_NOAA_normals_final.R
@@ -1,192 +1,255 @@
-# NCEI NOAA Gridded normals
-# NCEI.NOAA.GOV gridded normals
+#------------------------------------------------------------------------------------------
+# Code to compile NOAA gridded climate normals for NETN parks based on each park's
+# centroid. Note that ACAD and BOHA park centroids had to be adjusted to land in
+# NOAA grids. Grids were downloaded to local machine to speed up the process.
+#  # Normals were manually downloaded from:
+#     "https://www.ncei.noaa.gov/products/land-based-station/us-climate-normals"
+#  # Historic grids were downloaded from:
+#     "https://www.ncei.noaa.gov/thredds/catalog/data-in-development/nclimgrid/catalog.html
+#------------------------------------------------------------------------------------------
 
-library(terra)
+library(terra) # for normals
 library(sf)
 library(tidyverse)
-library(waterNETN)
-importData()
-path = "C:/NETN/R_Dev/Water/data/"
+path = "C:/NETN/R_Dev/climateNETN/data/"
 
-# Note I had issues with sf generating centroids, so I created them in ArcPro from NPS admin boundaries shapefile downloaded from IRMA.
+data("NETN_centroids")
+netn_sf <- st_as_sf(NETN_centroids, coords = c("long", "lat"), crs = 4326)
+netn_bbox <- netn_sf |> st_buffer(10000) |> st_bbox() #10km buffer so ACAD not cutoff
 
-# NETN bounding box
-NETN_bbox <- data.frame(lat = c(47.38, 44.80, 38.71, 43.40, 39.994187),
-                        long = c(-68.71, -66.67, -74.84, -75.54, -80.521832)) |>
-  st_as_sf(coords = c("long", "lat"), crs = 4326) |> st_bbox()
-# last point is farthest south and west, so region doesn't look as weird.
-
-netn_cent <- read.csv("../data/NPS_NETN_centroids_wgs84.txt") |>
-  select(UnitCode = UNIT_CODE, UnitName = UNIT_NAME, long = POINT_X, lat = POINT_Y) |>
-  arrange(UnitCode)
-
-netn_cent$UnitCode[netn_cent$UnitCode == "HOFR"] <- "ROVA"
-netn_cent$UnitName[netn_cent$UnitCode == "ROVA"] <- "Roosevelt-Vanderbilt National Historic Sites"
-netn_cent$long[netn_cent$UnitCode == "ACAD"] <- -68.260190 # had to move b/c landed in water
-netn_cent$lat[netn_cent$UnitCode == "ACAD"] <- 44.376559 # had to move b/c landed in water
-netn_cent$long[netn_cent$UnitCode == "BOHA"] <- -71.0215867 # had to move b/c landed in water
-netn_cent$lat[netn_cent$UnitCode == "BOHA"] <- 42.2709746 # had to move b/c landed in water
-write.csv(netn_cent, paste0(path, "NETN_park_centroids_wgs84.csv"), row.names = F)
-NETN_centroids <- netn_cent
-#NETN_cent_sf <- netn_cent |> st_as_sf(coords = c("long", "lat"), crs = 4326)
-usethis::use_data(NETN_centroids, overwrite = T)
-
-# Normals were manually downloaded from:
-  # "https://www.ncei.noaa.gov/products/land-based-station/us-climate-normals"
-
-# Extract 1991-2020 climate data for NETN park centroids
+#---- Extract NOAA Climate Normals for NETN parks ----
+#------ 1991-2020 Normals ------
 ppt30norm <- rast("../data/NRCC/prcp-1991_2020-monthly-normals-v1.0.nc")
-
-# wind <- extent(-77, -65, 38, 48)
-# raster::plot(wind)
-# plot(ppt30norm[[1]], add = T)
-# plot(NETN_centroids[1], add = T, pch = 20)
-
-ppt30 <- terra::extract(ppt30norm, netn_cent[,c("long", "lat")])
-ppt30$UnitCode = netn_cent$UnitCode
-ppt30_long <- ppt30 |> select(UnitCode, mlyprcp_norm_1:mlyprcp_norm_12, mlyprcp_std_1:mlyprcp_std_12) |>
+ppt30 <- terra::extract(ppt30norm, NETN_centroids[,c("long", "lat")])
+ppt30$UnitCode = NETN_centroids$UnitCode
+ppt30_long <- ppt30 |> dplyr::select(UnitCode, mlyprcp_norm_1:mlyprcp_norm_12, mlyprcp_std_1:mlyprcp_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlyprcp") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("precip_", stat),
          norm = "norm1991-2020") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlyprcp)
 
 tmax30norm <- rast("../data/NRCC/tmax-1991_2020-monthly-normals-v1.0.nc")
-tmax30 <- terra::extract(tmax30norm, netn_cent[,c("long", "lat")])
-tmax30$UnitCode = netn_cent$UnitCode
-tmax30_long <- tmax30 |> select(UnitCode, mlytmax_norm_1:mlytmax_norm_12, mlytmax_std_1:mlytmax_std_12) |>
+tmax30 <- terra::extract(tmax30norm, NETN_centroids[,c("long", "lat")])
+tmax30$UnitCode = NETN_centroids$UnitCode
+tmax30_long <- tmax30 |> dplyr::select(UnitCode, mlytmax_norm_1:mlytmax_norm_12, mlytmax_std_1:mlytmax_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytmax") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tmax_", stat),
          norm = "norm1991-2020") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytmax)
-head(tmax30_long)
 
 tmin30norm <- rast("../data/NRCC/tmin-1991_2020-monthly-normals-v1.0.nc")
-tmin30 <- terra::extract(tmin30norm, netn_cent[,c("long", "lat")])
-tmin30$UnitCode = netn_cent$UnitCode
-tmin30_long <- tmin30 |> select(UnitCode, mlytmin_norm_1:mlytmin_norm_12, mlytmin_std_1:mlytmin_std_12) |>
+tmin30 <- terra::extract(tmin30norm, NETN_centroids[,c("long", "lat")])
+tmin30$UnitCode = NETN_centroids$UnitCode
+tmin30_long <- tmin30 |> dplyr::select(UnitCode, mlytmin_norm_1:mlytmin_norm_12, mlytmin_std_1:mlytmin_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytmin") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tmin_", stat),
          norm = "norm1991-2020") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytmin)
-head(tmin30_long)
 
 tavg30norm <- rast("../data/NRCC/tavg-1991_2020-monthly-normals-v1.0.nc")
-tavg30 <- terra::extract(tavg30norm, netn_cent[,c("long", "lat")])
-tavg30$UnitCode = netn_cent$UnitCode
-tavg30_long <- tavg30 |> select(UnitCode, mlytavg_norm_1:mlytavg_norm_12, mlytavg_std_1:mlytavg_std_12) |>
+tavg30 <- terra::extract(tavg30norm, NETN_centroids[,c("long", "lat")])
+tavg30$UnitCode = NETN_centroids$UnitCode
+tavg30_long <- tavg30 |> dplyr::select(UnitCode, mlytavg_norm_1:mlytavg_norm_12, mlytavg_std_1:mlytavg_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytavg") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tavg_", stat),
          norm = "norm1991-2020") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytavg)
-head(ppt30_long)
-head(tmax30_long)
+
 norm30 <- list(ppt30_long, tmax30_long, tmin30_long, tavg30_long)
 comb30 <- reduce(norm30, full_join, by = c("UnitCode", "month", "norm"))
 head(comb30)
 
-# Extract 1901-2000 climate data for NETN park centroids
+#------ 1901-2000 Normals ------
 ppt19norm <- rast("../data/NRCC/prcp-1901_2000-monthly-normals-v1.0.nc")
-st_crs(ppt19norm)
 
-ppt <- terra::extract(ppt19norm, netn_cent[,c("long", "lat")])
-ppt$UnitCode = netn_cent$UnitCode
-ppt_long <- ppt |> select(UnitCode, mlyprcp_norm_1:mlyprcp_norm_12, mlyprcp_std_1:mlyprcp_std_12) |>
+ppt <- terra::extract(ppt19norm, NETN_centroids[,c("long", "lat")])
+ppt$UnitCode = NETN_centroids$UnitCode
+ppt_long <- ppt |> dplyr::select(UnitCode, mlyprcp_norm_1:mlyprcp_norm_12, mlyprcp_std_1:mlyprcp_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlyprcp") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("precip_", stat),
          norm = "norm1901-2000") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlyprcp)
 
-
 tmax19norm <- rast("../data/NRCC/tmax-1901_2000-monthly-normals-v1.0.nc")
-tmax <- terra::extract(tmax19norm, netn_cent[,c("long", "lat")])
-tmax$UnitCode = netn_cent$UnitCode
-tmax_long <- tmax |> select(UnitCode, mlytmax_norm_1:mlytmax_norm_12, mlytmax_std_1:mlytmax_std_12) |>
+tmax <- terra::extract(tmax19norm, NETN_centroids[,c("long", "lat")])
+tmax$UnitCode = NETN_centroids$UnitCode
+tmax_long <- tmax |> dplyr::select(UnitCode, mlytmax_norm_1:mlytmax_norm_12, mlytmax_std_1:mlytmax_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytmax") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tmax_", stat),
          norm = "norm1901-2000") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytmax)
-head(tmax_long)
 
 tmin19norm <- rast("../data/NRCC/tmin-1901_2000-monthly-normals-v1.0.nc")
-tmin <- terra::extract(tmin19norm, netn_cent[,c("long", "lat")])
-tmin$UnitCode = netn_cent$UnitCode
-tmin_long <- tmin |> select(UnitCode, mlytmin_norm_1:mlytmin_norm_12, mlytmin_std_1:mlytmin_std_12) |>
+tmin <- terra::extract(tmin19norm, NETN_centroids[,c("long", "lat")])
+tmin$UnitCode = NETN_centroids$UnitCode
+tmin_long <- tmin |> dplyr::select(UnitCode, mlytmin_norm_1:mlytmin_norm_12, mlytmin_std_1:mlytmin_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytmin") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tmin_", stat),
          norm = "norm1901-2000") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytmin)
-head(tmin_long)
 
 tavg19norm <- rast("../data/NRCC/tavg-1901_2000-monthly-normals-v1.0.nc")
-tavg <- terra::extract(tavg19norm, netn_cent[,c("long", "lat")])
-tavg$UnitCode = netn_cent$UnitCode
-tavg_long <- tavg |> select(UnitCode, mlytavg_norm_1:mlytavg_norm_12, mlytavg_std_1:mlytavg_std_12) |>
+tavg <- terra::extract(tavg19norm, NETN_centroids[,c("long", "lat")])
+tavg$UnitCode = NETN_centroids$UnitCode
+tavg_long <- tavg |> dplyr::select(UnitCode, mlytavg_norm_1:mlytavg_norm_12, mlytavg_std_1:mlytavg_std_12) |>
   pivot_longer(-UnitCode, names_to = "parameter", values_to = "mlytavg") |>
   mutate(month = as.numeric(gsub("\\D", "", parameter)),
          stat = ifelse(grepl("norm", parameter), "norm", "std"),
          param = paste0("tavg_", stat),
          norm = "norm1901-2000") |>
-  select(-parameter, -stat) |>
+  dplyr::select(-parameter, -stat) |>
   pivot_wider(names_from = param, values_from = mlytavg)
-
 
 norm19 <- list(ppt_long, tmax_long, tmin_long, tavg_long)
 comb19 <- reduce(norm19, full_join, by = c("UnitCode", "month", "norm"))
-head(comb19)
-head(comb30)
 
-comb_norms <- full_join(comb19 |> select(-norm), comb30 |> select(-norm),
+comb_norms <- full_join(comb19 |> dplyr::select(-norm), comb30 |> dplyr::select(-norm),
                         by = c("UnitCode", "month"),
                         suffix = c("_1901_2000", "_1991_2020"))
-head(comb_norms)
 
-NETN_clim_norms <- left_join(netn_cent, comb_norms, by = "UnitCode")
+NETN_clim_norms <- left_join(NETN_centroids, comb_norms, by = "UnitCode")
+head(NETN_clim_norms)
+names(NETN_clim_norms) <- gsub("precip", "ppt", names(NETN_clim_norms))
+names(NETN_clim_norms) <- gsub("tavg", "tmean", names(NETN_clim_norms))
 
-#NETN_clim_norms <- read.csv("../data/NETN_clim_norms.csv")
-#NETN_clim_norms <- NETN_clim_norms |> rename(UnitCode = UNIT_CODE)
-
-# data("NETN_clim_norms")
-# NETN_clim_norms <- NETN_clim_norms |> rename(UnitCode = UNIT_CODE)
 write.csv(NETN_clim_norms, "../data/NETN_clim_norms.csv")
-usethis::use_data(NETN_clim_norms, overwrite = T)
+#usethis::use_data(NETN_clim_norms, overwrite = T)
 
-# Add temps as F and precip as "in"
-data("NETN_clim_norms")
-c_to_f <- function(x){
-  (x * 9/5) + 32
+head(NETN_clim_norms)
+#---- Compile error around normals ----
+#library(raster) # for historic grids
+
+compnoaa <- function(yr, mon){
+  bnd <- (yr - 1895) * 12 + mon
+  prcp <- raster::raster("../data/ncei/nclimgrid_prcp.nc", band = bnd)
+  prcp_crop <- raster::crop(prcp, netn_bbox)
+  netn_prcp <- cbind(NETN_centroids, prcp = raster::extract(prcp_crop, NETN_centroids[,c("long", "lat")]))
+
+  tmax <- raster::raster("../data/ncei/nclimgrid_tmax.nc", band = bnd)
+  tmax_crop <- raster::crop(tmax, netn_bbox)
+  netn_tmax <- cbind(NETN_centroids, tmax = raster::extract(tmax_crop, NETN_centroids[,c("long", "lat")]))
+
+  tmin <- raster::raster("../data/ncei/nclimgrid_tmin.nc", band = bnd)
+  tmin_crop <- raster::crop(tmin, netn_bbox)
+  netn_tmin <- cbind(NETN_centroids, tmin = raster::extract(tmin_crop, NETN_centroids[,c("long", "lat")]))
+
+  tavg <- raster::raster("../data/ncei/nclimgrid_tavg.nc", band = bnd)
+  tavg_crop <- raster::crop(tavg, netn_bbox)
+  netn_tavg <- cbind(NETN_centroids, tavg = raster::extract(tavg_crop, NETN_centroids[,c("long", "lat")]))
+
+  clim_list <- list(netn_prcp, netn_tmax, netn_tmin, netn_tavg)
+  netn_comb <- purrr::reduce(clim_list, full_join, by = c("UnitCode", "UnitName", "long", "lat"))
+
+  netn_comb$year = yr
+  netn_comb$month = as.numeric(mon)
+
+  return(data.frame(netn_comb))
 }
 
-mm_to_in <- function(x){
-  (x / 25.4)
-}
+#------ 1991-2020 Error------
+years <- 1991:2020
+months <- 1:12
+yrmon <- expand.grid(year = years, mon =  months)
 
-NETN_clim_norms <- NETN_clim_norms |> mutate(across(.cols = starts_with("t"),
-                                                    .fns = ~c_to_f(.x),
-                                                    .names = "{.col}_F"))
+netn_annual <- purrr::map2(yrmon$year, yrmon$mon, function(x, y){
+  compnoaa(yr = x, mon = y)}, .progress = T) |> list_rbind()
 
-NETN_clim_norms <- NETN_clim_norms |> mutate(across(.cols = starts_with("precip"),
-                                                    .fns = ~mm_to_in(.x),
-                                                    .names = "{.col}_in"))
+names(netn_annual) <- gsub("prcp", "ppt", names(netn_annual))
+names(netn_annual) <- gsub("tavg", "tmean", names(netn_annual))
+head(netn_annual)
 
+netn_sum_1991_2020 <- netn_annual |> group_by(UnitCode, month) |>
+  summarize(ppt_min_1991_2020 = min(ppt),
+            ppt_max_1991_2020 = max(ppt),
+            ppt_u95_1991_2020 = quantile(ppt, 0.975),
+            ppt_l95_1991_2020 = quantile(ppt, 0.025),
+            ppt_u50_1991_2020 = quantile(ppt, 0.75),
+            ppt_l50_1991_2020 = quantile(ppt, 0.25),
+            tmean_min_1991_2020 = min(tmean),
+            tmean_max_1991_2020 = max(tmean),
+            tmean_u95_1991_2020 = quantile(tmean, 0.975),
+            tmean_l95_1991_2020 = quantile(tmean, 0.025),
+            tmean_u50_1991_2020 = quantile(tmean, 0.75),
+            tmean_l50_1991_2020 = quantile(tmean, 0.25),
+            tmin_min_1991_2020 = min(tmin),
+            tmin_max_1991_2020 = max(tmin),
+            tmin_u95_1991_2020 = quantile(tmin, 0.975),
+            tmin_l95_1991_2020 = quantile(tmin, 0.025),
+            tmin_u50_1991_2020 = quantile(tmin, 0.75),
+            tmin_l50_1991_2020 = quantile(tmin, 0.25),
+            tmax_min_1991_2020 = min(tmax),
+            tmax_max_1991_2020 = max(tmax),
+            tmax_u95_1991_2020 = quantile(tmax, 0.975),
+            tmax_l95_1991_2020 = quantile(tmax, 0.025),
+            tmax_u50_1991_2020 = quantile(tmax, 0.75),
+            tmax_l50_1991_2020 = quantile(tmax, 0.25),
+            .groups = 'drop')
+
+head(netn_sum_1991_2020)
+#------ 1901-2000 Error------
+years <- 1901:2000
+months <- 1:12
+yrmon <- expand.grid(year = years, mon =  months)
+
+netn_annual <- purrr::map2(yrmon$year, yrmon$mon, function(x, y){
+  compnoaa(yr = x, mon = y)}, .progress = T) |> list_rbind()
+
+names(netn_annual) <- gsub("prcp", "ppt", names(netn_annual))
+names(netn_annual) <- gsub("tavg", "tmean", names(netn_annual))
+head(netn_annual)
+
+netn_sum_1901_2000 <- netn_annual |> group_by(UnitCode, month) |>
+  summarize(ppt_min_1901_2000 = min(ppt),
+            ppt_max_1901_2000 = max(ppt),
+            ppt_u95_1901_2000 = quantile(ppt, 0.975),
+            ppt_l95_1901_2000 = quantile(ppt, 0.025),
+            ppt_u50_1901_2000 = quantile(ppt, 0.75),
+            ppt_l50_1901_2000 = quantile(ppt, 0.25),
+            tmean_min_1901_2000 = min(tmean),
+            tmean_max_1901_2000 = max(tmean),
+            tmean_u95_1901_2000 = quantile(tmean, 0.975),
+            tmean_l95_1901_2000 = quantile(tmean, 0.025),
+            tmean_u50_1901_2000 = quantile(tmean, 0.75),
+            tmean_l50_1901_2000 = quantile(tmean, 0.25),
+            tmin_min_1901_2000 = min(tmin),
+            tmin_max_1901_2000 = max(tmin),
+            tmin_u95_1901_2000 = quantile(tmin, 0.975),
+            tmin_l95_1901_2000 = quantile(tmin, 0.025),
+            tmin_u50_1901_2000 = quantile(tmin, 0.75),
+            tmin_l50_1901_2000 = quantile(tmin, 0.25),
+            tmax_min_1901_2000 = min(tmax),
+            tmax_max_1901_2000 = max(tmax),
+            tmax_u95_1901_2000 = quantile(tmax, 0.975),
+            tmax_l95_1901_2000 = quantile(tmax, 0.025),
+            tmax_u50_1901_2000 = quantile(tmax, 0.75),
+            tmax_l50_1901_2000 = quantile(tmax, 0.25),
+            .groups = 'drop')
+
+df_list <- list(NETN_clim_norms, netn_sum_1901_2000, netn_sum_1991_2020)
+NETN_clim_comb1 <- purrr::reduce(df_list, left_join, by = c("UnitCode", "month"))
+
+# dropping the std columns for now, though code above extracted them.
+names(NETN_clim_comb)
+NETN_clim_norms <- NETN_clim_comb1[,-grep("std", colnames(NETN_clim_comb1))]
 usethis::use_data(NETN_clim_norms, overwrite = T)
